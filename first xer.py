5b417897aa5277276570c87b0c7af824a6649a8c
Nazar-taraniuk <nazar.taraniuk@gmail.com>
2024-09-09 18:25:46 +0300

    Add files via upload

---
+++ first xer.py
@@ -0,0 +1,10 @@
+mark = "hello world"
+print (mark)
+print (type(mark))
+name = "Назар"
+second_name = "Таранюк"
+print (name, second_name)
+print (type(name))
+age = 16
+print(age)
+print (type (age))
