25652b914d2c06037bb293fcc7916564741a53a0
Nazar-taraniuk <nazar.taraniuk@gmail.com>
2024-11-17 22:39:29 +0200

    Delete lilpeep directory

--- lilpeep/bot.py
+++
@@ -1,9 +0,0 @@
-from config import bot
-from handlers import handle_start, callback_handler
-from parser import req
-
-req()
-
-if __name__ == "__main__":
-    print("bot ready")
-    bot.polling(none_stop=True)
